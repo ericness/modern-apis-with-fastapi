2a6f9d01ee75345201e32257ecd604811df16eb2
nesseri <eric.ness@chrobinson.com>
2020-12-17 09:07:30 -0600

    Add homepage

--- ch03-first-api/main_ericness.py
+++ ch03-first-api/main_ericness.py
@@ -6,12 +6,27 @@ import uvicorn
 api = fastapi.FastAPI()
 
 
+@api.get("/")
+def index():
+    content = """
+    <html>
+    <body style='padding: 10px;'>
+    <h1>Welcome to the API</h1>
+    <div>
+    Try it: <a href='/api/calculate?x=7&y=11'>/api/calculate?x=7&y=11</a>
+    </div>
+    </body>
+    </html>
+    """
+
+    return fastapi.responses.HTMLResponse(content=content)
+
+
 @api.get("/api/calculate")
 def calculate(x: int, y: int, z: Optional[int] = None):
     if z == 0:
         return fastapi.responses.JSONResponse(
-            content={"error": "z cannot be zero"},
-            status_code=400,
+            content={"error": "z cannot be zero"}, status_code=400,
         )
 
     result = x + y
