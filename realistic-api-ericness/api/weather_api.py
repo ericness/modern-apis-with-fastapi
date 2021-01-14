3f74c852e9ff0de4de9d74ae57851b39c10dd1e8
nesseri <eric.ness@chrobinson.com>
2021-01-14 09:18:07 -0600

    Call OpenWeather service

--- realistic-api-ericness/api/weather_api.py
+++ realistic-api-ericness/api/weather_api.py
@@ -4,6 +4,7 @@ import fastapi
 from fastapi import Depends
 
 from models.location import Location
+from services import openweather_service
 
 router = fastapi.APIRouter()
 
@@ -13,4 +14,6 @@ def weather(
     loc: Location = Depends(),
     units: Optional[str] = "metric",
 ):
-    return f"{loc.city}, {loc.state}, {loc.country} in {units}"
+    report = openweather_service.get_report(loc.city, loc.state, loc.country, units)
+
+    return report
